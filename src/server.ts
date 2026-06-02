dbe9f38d2cab4da9ecb8d6ec5b00e6410d422d65
iamapusaha <apusaha392@gmail.com>
2026-06-02 11:46:23 +0600

    added get and post method

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
-import express from "express";
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
 const port = 5000;
-app.get("/", (req, res) => {
-  res.send(
-    "yes,we are live! yes we are finally as always hello vai ki obesta, ki koren",
-  );
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "server up to date",
+    author: "Apu Saha",
+  });
+});
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
