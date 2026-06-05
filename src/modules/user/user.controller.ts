88929c34ad7b71a326591e173cd862f8ada8415e
iamapusaha <apusaha392@gmail.com>
2026-06-05 09:54:06 +0600

    post method complete

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,16 +1,9 @@
 import type { Request, Response } from "express";
-import { pool } from "../../db";
+import { UserService } from "./user.service";
 
 const createUser = async (req: Request, res: Response) => {
-  const { name, email, password, age } = req.body;
   try {
-    const result = await pool.query(
-      `
-    INSERT INTO users(name,email,password,age) VALUES($1,$2,$3,$4)
-      RETURNING *
-    `,
-      [name, email, password, age],
-    );
+    const result = await UserService.createUserInToDB(req.body);
     console.log(result);
     res.status(201).json({
       message: "data create successfully",
---
+++ src/modules/user/user.interface.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  name: string;
+  password: string;
+  email: string;
+  age: number;
+  is_active?: boolean;
+}
