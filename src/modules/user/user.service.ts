354cf36eb3ef664295ae4b93118615044832c10f
iamapusaha <apusaha392@gmail.com>
2026-06-07 10:11:45 +0600

    added hash passeord for user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,15 +1,18 @@
+import bcrypt from "bcrypt";
 import { pool } from "../../db";
 import type { IUser } from "./user.interface";
 
 const createUserInToDB = async (payload: IUser) => {
   const { name, email, password, age } = payload;
+  const hashPassword = await bcrypt.hash(password, 10);
   const result = await pool.query(
     `
         INSERT INTO users(name,email,password,age) VALUES($1,$2,$3,$4)
           RETURNING *
         `,
-    [name, email, password, age],
+    [name, email, hashPassword, age],
   );
+  delete result.rows[0].password;
   return result;
 };
 
